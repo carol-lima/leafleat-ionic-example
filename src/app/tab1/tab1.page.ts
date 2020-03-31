b9a0ac25aaae2f1e20c1b3609832e2e632fb818a
Carol Lima <carollima.dev@gmail.com>
2020-03-31 18:20:59 -0300

    Initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  map: Map;
 
-  constructor() {}
+  ionViewDidEnter() { this.leafletMap(); }
 
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId').setView([-23.5629, -46.6544], 10); 
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'CL',
+    }).addTo(this.map);
+
+    marker([-23.5629, -46.6544]).addTo(this.map)
+      .bindPopup('Av Paulista')
+      .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 }
